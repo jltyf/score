b7d710a720bed6000b6c30dca3a4709d83405632
汤宇飞 <jltyf@sina.com>
2022-07-29 15:39:05 +0800

    aeblka1

--- script/aeblka_1.py
+++ script/aeblka_1.py
@@ -10,6 +10,8 @@ def get_score(xml_path, weather_result):
     veh_speed = 500
     veh_acc_target = 500
     veh_acc = 1000
+    ego_car_type = None
+    veh_car_type = None
     require_weather = Weather.SNOW
     if require_weather == weather_result:
         weather_score = True
@@ -24,15 +26,16 @@ def get_score(xml_path, weather_result):
         for player in root.iter('Player'):
             for des in player.iter('Description'):
                 if des.attrib['Name'] == 'Ego':
+                    ego_car_type = des.attrib['Type']
                     for ego_init in player.iter('Init'):
                         for ego_data in ego_init:
                             if ego_data.tag == 'Speed':
                                 ego_speed = float(ego_data.attrib['Value'])
                             elif ego_data.tag == 'PosAbsolute':
                                 ego_direction = float(ego_data.attrib['Direction'])
-                                ego_start_x = float(ego_data.attrib['X'])
 
                 elif des.attrib['Name'] == 'veh_1':
+                    veh_car_type = des.attrib['Type']
                     for veh_init in player.iter('Init'):
                         for veh_data in veh_init:
                             if veh_data.tag == 'Speed':
@@ -46,28 +49,18 @@ def get_score(xml_path, weather_result):
                     veh_acc = float(speed_change.attrib['Rate'])
                     veh_acc_target = float(speed_change.attrib['Target'])
 
-        if ego_speed == 80 / 3.6:
-            xml_score_detail = f'{item}.测试车初始速度等于80km/h,得2分;<br/>'
-            score += 2
-        else:
-            xml_score_detail = f'{item}.测试车初始速度不等于80km/h,不得分;<br/>'
-        item += 1
-        if ego_direction == math.radians(5):
-            score += 2
-            xml_score_detail = xml_score_detail + f'{item}.测试车初始车头方向偏离车道5°,得2分;<br/>'
-        else:
-            xml_score_detail = xml_score_detail + f'{item}.测试车初始车头方向偏离车道不等于5°,不得分;<br/>'
-        item += 1
-        if veh_speed == 40 / 3.6 and abs(distance) == 300:
+        if ego_speed == 80 / 3.6 and ego_direction == math.radians(5) and ego_car_type == 'CICV_Car':
             score += 1
-            xml_score_detail = xml_score_detail + '3.目标车初始速度等于40km/h且与测试车车距等于300m,得1分;<br/>'
+            xml_score_detail = f'{item}.测试车(Ego)车型为CICV_Car,以80km/h,初始车头方向偏离车道5°,沿直道行驶,且不驶出本车道,得1分;<br/>'
         else:
-            xml_score_detail = xml_score_detail + '3.不满足目标车初始速度等于40km/h且与测试车车距等于300m,不得分;<br/>'
-        if veh_acc == 3 and veh_acc_target == 0:
+            xml_score_detail = f'{item}.不满足测试车(Ego)车型为CICV_Car,以80km/h,初始车头方向偏离车道5°,沿直道行驶,且不驶出本车道,不得分;<br/>'
+        item += 1
+        if 19.99 / 3.6 <= veh_speed <= 40.01 / 3.6 and 150 <= abs(
+                distance) <= 200 and veh_acc <= 5 and veh_acc_target == 0 and veh_car_type == 'Audi_A3_2009_red':
             score += 1
-            xml_score_detail = xml_score_detail + '4.目标车减速度等于3m/s²且减速至静止,得1分;<br/>'
+            xml_score_detail = xml_score_detail + f'{item}.测试车前方150-200m有车辆(veh_1),车型为Audi_A3_2009_red,以初速度20-40km/h,减速度不超过5m/s²,减速至静止,得1分;<br/>'
         else:
-            xml_score_detail = xml_score_detail + '4.不满足目标车减速度等于3m/s²且减速至静止,不得分;<br/>'
+            xml_score_detail = xml_score_detail + f'{item}.不满足测试车前方150-200m有车辆(veh_1),车型为Audi_A3_2009_red,以初速度20-40km/h,减速度不超过5m/s²,减速至静止,不得分;<br/>'
         item += 1
         return score, xml_score_detail, weather_score, item
     except:
